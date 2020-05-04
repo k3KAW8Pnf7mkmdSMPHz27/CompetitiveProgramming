15c57690d24298c58520a1c8fbedc94ef5b2ba64
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2020-05-04 16:02:27 -0400

    COVID19 problem for CodeChef May challenge CORUS input done

---
+++ CodeChefMAY20B/CORUS.java
@@ -0,0 +1,42 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CORUS {
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = readInt(br);
+        for (; T > 0; T--) {
+            int N = readInt(br);
+            int Q = readInt(br);
+            br.read(); //Remove new line
+            char[] S = br.readLine().toCharArray();
+            // S is of length N
+            // Each ith person has disease S_i
+            int[] carriers = new int[26]; //Given by problem, max 26
+
+            for (int i = 0; i < Q; i++) {
+                int C = readInt(br);
+
+            }
+        }
+    }
+
+    private static int readInt(BufferedReader br) throws IOException {
+        int value = 0;
+        int current = br.read();
+        while (current < '0' || current > '9') {
+            current = br.read();
+        }
+        while (current >= '0' && current <= '9') {
+            value *= 10;
+            value += current - '0';
+            current = br.read();
+        }
+        return value;
+    }
+}
